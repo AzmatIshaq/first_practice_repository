3331abecbe476d6a1d8f3aab652f8033dfa65230
Azmat Ishaq <azmatishaq@outlook.com>
2022-04-23 22:14:05 -0400

    Updates

---
+++ summer-practice-2022/js/script.js
@@ -0,0 +1,105 @@
+/**
+Project title
+Author
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+/* - - - - - - - - - - - VARIABLES - - - - - - - - - - - - - - - - - - - - -  */
+
+// Variable to style canvas
+let canvasProperties = {
+// Set canvas width and height
+  w: 850,
+  h: 550,
+};
+
+// Variable to style the background
+
+let bg = {
+  r: 0,
+  g: 0,
+  b: 0,
+};
+
+// Variable to set starting state to `title`
+
+let state = `title`;
+
+
+/*********************** PRELOAD **********************************************/
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/*********************** SETUP ************************************************/
+
+/**
+Description of setup
+*/
+function setup() {
+
+  createCanvas(canvasProperties.w, canvasProperties.h);
+
+}
+
+
+/*********************** DRAW *************************************************/
+
+
+/**
+Description of draw
+*/
+function draw() {
+  background(bg.r, bg.g, bg.b);
+
+    // Alternate between game states
+  if (state === `title`) {
+    titleState();
+  }
+  if (state === `animation`) {
+    animationState();
+  }
+
+} // End of draw()
+
+/* - - - - - - - - - - - USER INTERACTION - - - - - - - - - - - - - - - - - - */
+
+/*********************** KEY PRESSED ******************************************/
+
+// function keyPressed() {
+// }
+
+/*********************** MOUSE PRESSED ****************************************/
+
+// function mousePressed() {
+// }
+
+/* - - - - - - - - - - - STATES - - - - - - - - - - - - - - - - - - - - - - - */
+
+/*********************** ANIMATION STATE **************************************/
+
+function animationState() {
+
+}
+
+/*********************** TITLE STATE ******************************************/
+
+function titleState() {
+
+}
+
+/*********************** RESET STATES *****************************************/
+
+function resetStates() {
+
+}
